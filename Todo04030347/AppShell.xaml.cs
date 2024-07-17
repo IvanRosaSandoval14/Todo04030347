8e3872be68bd817f016bfb90d2d374f5a1f7b1bc
Ivansandoval14 <160464805+Ivansandoval14@users.noreply.github.com>
2024-07-17 10:31:18 -0600

    Agregar archivos de proyecto.

---
+++ Todo04030347/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Todo04030347.Views;
+
+namespace Todo04030347
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+        }
+    }
+}
---
+++ Todo04030347/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Todo04030347.Data;
+using Todo04030347.Views;
+
+namespace Todo04030347
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddSingleton<TodoListPage>();
+            builder.Services.AddTransient<TodoItemPage>();
+
+            builder.Services.AddSingleton<TodoItemDatabase>();
+
+            return builder.Build();
+        }
+    }
+}
